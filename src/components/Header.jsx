3b5520526ddb34c264c29d7c6eaaac6b94c77f4b
Nijel <nmanalo543@gmail.com>
2022-07-30 23:39:44 +0100

    add shadow to header + changed button text to white

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,14 +5,14 @@ function Header() {
   const user = false;
 
   return (
-    <nav className="bg-cyan-600">
+    <nav className="bg-cyan-600 drop-shadow-xl">
       <div className="container mx-auto p-10 flex justify-between items-center">
         <div>
           <Link to="/" className="text-3xl font-bold hover:text-gray-700">
             ThoughtScape
           </Link>
         </div>
-        <div className="flex flex-col space-y-3 md:space-y-0 md:flex-row md:space-x-5">
+        <div className="flex flex-col space-y-3 md:space-y-0 md:flex-row md:space-x-5 text-white">
           {!!user ? (
             <button className="flex items-center space-x-2 font-bold bg-red-500 px-6 py-3 rounded-lg drop-shadow-md hover:bg-red-600">
               <FaSignOutAlt />
