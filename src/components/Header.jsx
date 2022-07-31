bf8e7bd7848e7896319e4c0df96537d0164240d6
Nijel Manalo <nmanalo543@gmail.com>
2022-07-31 13:21:34 +0100

    make tailwind component for header buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,23 +8,23 @@ function Header() {
     <nav className="bg-cyan-600 drop-shadow-xl">
       <div className="container mx-auto p-10 flex justify-between items-center">
         <div>
-          <Link to="/" className="text-3xl font-bold hover:text-gray-700">
+          <Link to="/" className="text-4xl font-bold hover:text-gray-700 overline">
             ThoughtScape
           </Link>
         </div>
-        <div className="flex flex-col space-y-3 md:space-y-0 md:flex-row md:space-x-5 text-white">
+        <div className="flex flex-col space-y-3 md:space-y-0 md:flex-row md:space-x-5">
           {!!user ? (
-            <button className="flex items-center space-x-2 font-bold bg-red-500 px-6 py-3 rounded-lg drop-shadow-md hover:bg-red-600">
+            <button className="header-btn">
               <FaSignOutAlt />
               <span>sign out</span>
             </button>
           ) : (
             <>
-              <button className="flex items-center space-x-2 font-bold bg-red-500 px-6 py-3 rounded-lg drop-shadow-md hover:bg-red-600">
+              <button className="header-btn">
                 <FaSignInAlt />
                 <span>sign in</span>
               </button>
-              <button className="flex items-center space-x-2 font-bold bg-red-500 px-6 py-3 rounded-lg drop-shadow-md hover:bg-red-600">
+              <button className="header-btn">
                 <FaUser />
                 <span>register</span>
               </button>
