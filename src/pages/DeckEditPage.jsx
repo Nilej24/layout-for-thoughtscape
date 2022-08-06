ef77b6c976f2fcec72223fccdf438220d2097ca6
Nijel Manalo <nmanalo543@gmail.com>
2022-08-06 01:52:47 +0100

    added to deck edit page; also idk why i even made this separate repo so i will move everything into the actual one

--- src/pages/DeckEditPage.jsx
+++ src/pages/DeckEditPage.jsx
@@ -1,17 +1,36 @@
+import { FaEdit } from 'react-icons/fa';
+
 function DeckEditPage() {
-  const deckName = 'Phyisics is shit';
+  const deckName = 'Physics is shit';
 
   return (
     <>
-      <h1 className="text-3xl font-semibold text-center">
-        {deckName}
+      <h1 className="text-3xl font-semibold flex justify-center items-center space-x-4 py-7 px-5">
+        <span>
+          deck: {deckName}
+        </span>
+        <button className="rounded-md text-xl p-2 bg-red-500 border-2 border-black hover:bg-red-600">
+          <FaEdit />
+        </button>
       </h1>
-      <div className="container mx-auto grid grid-rows-1 grid-cols-2">
-        <section className="text-center bg-red-500">
-          sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+      <div className="container mx-auto flex flex-col-reverse md:flex-row">
+        <section className="text-center w-full md:w-1/2">
+          <h2 className="text-xl font-medium text-center">
+            cards
+          </h2>
+          <div className="flex font-light">
+            <h3 className="text-lg text-center w-1/2">
+              question
+            </h3>
+            <h3 className="text-lg text-center w-1/2">
+              answer
+            </h3>
+          </div>
         </section>
-        <section className="text-center bg-blue-500">
-          s
+        <section className="text-center w-full md:w-1/2">
+          <h2 className="text-xl font-medium text-center">
+            edit card
+          </h2>
         </section>
       </div>
     </>
